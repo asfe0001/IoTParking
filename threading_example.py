9631a1dd819383e8a0ad9e5fdad6f1d198c758e2
Ferhat Aslan <ferhat.aslan@hs-karlsruhe.de>
2021-06-07 15:01:40 +0200

    erweiterung auf 3 threads

--- threading_example.py
+++ threading_example.py
@@ -3,7 +3,7 @@ import time
 
 exitFlag = 0
 
-class myThread (threading.Thread):
+class garageThread (threading.Thread):
    def __init__(self, threadID, name, counter):
       threading.Thread.__init__(self)
       self.threadID = threadID
@@ -11,23 +11,69 @@ class myThread (threading.Thread):
       self.counter = counter
    def run(self):
       print ("Starting " + self.name)
-      print_time(self.name, 5, self.counter)
+      subGarageState(self.name, 5, self.counter)
       print ("Exiting " + self.name)
 
-def print_time(threadName, counter, delay):
+
+
+class carThread (threading.Thread):
+   def __init__(self, threadID, name, counter):
+      threading.Thread.__init__(self)
+      self.threadID = threadID
+      self.name = name
+      self.counter = counter
+   def run(self):
+      print ("Starting " + self.name)
+      subCar(self.name, 5, self.counter)
+      print ("Exiting " + self.name)
+      
+      
+class AssignmentThread (threading.Thread):
+   def __init__(self, threadID, name, counter):
+      threading.Thread.__init__(self)
+      self.threadID = threadID
+      self.name = name
+      self.counter = counter
+   def run(self):
+      print ("Starting " + self.name)
+      pubCar(self.name, 5, self.counter)
+      print ("Exiting " + self.name)
+
+def subGarageState(threadName, counter, delay):
    while counter:
       if exitFlag:
          threadName.exit()
       time.sleep(delay)
       print ("%s: %s" % (threadName, time.ctime(time.time())))
       counter -= 1
+      
+      
+def subCar(threadName, counter, delay):
+   while counter:
+      if exitFlag:
+         threadName.exit()
+      time.sleep(delay)
+      print ("%s: %s" % (threadName, time.ctime(time.time())))
+      counter -= 1 
+      
+      
+def pubCar(threadName, counter, delay):
+   while counter:
+      if exitFlag:
+         threadName.exit()
+      time.sleep(delay)
+      print ("%s: %s" % (threadName, time.ctime(time.time())))
+      counter -= 1     
+      
 
 # Create new threads
-thread1 = myThread(1, "Thread-1", 1)
-thread2 = myThread(2, "Thread-2", 2)
+thread1 = garageThread(1, "Thread-1", 1)
+thread2 = carThread(2, "Thread-2", 2)
+thread3 = AssignmentThread(3, "Thread-3", 3)
 
 # Start new Threads
 thread1.start()
 thread2.start()
+thread3.start()
 
 print ("Exiting Main Thread")
